8ff412f8bcea6b8cd81fbcc34240760d9086c04e
Rafael Manfrim <rafaelmanfrim2004@gmail.com>
2025-03-24 20:13:07 -0300

    Materiais e alguns exercícios atrasados

---
+++ aula08/Ex01.java
@@ -0,0 +1,12 @@
+// • Escreva um programa que imprima 'Olá, Mundo!' na tela.
+// • Utilize comentários para descrever o que seu código faz.
+// • Certifique-se de que seu código esteja formatado corretamente.
+
+package aula08;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        // Imprime 'Olá, Mundo!' na tela
+        System.out.println("Olá, Mundo!");
+    }
+}
---
+++ aula08/Ex02.java
@@ -0,0 +1,14 @@
+// • Declare uma variável do tipo inteiro e atribua um valor.
+// • Crie uma variável do tipo double para armazenar um número real.
+// • Imprima os valores das variáveis na tela.
+
+package aula08;
+
+public class Ex02 {
+  public static void main(String[] args) {
+    int n1 = 5;
+    double n2 = 3.14;
+    System.out.println("n1: " + n1);
+    System.out.println("n2: " + n2);
+  }
+}
